4cef8f686cb45a1d9d69bff3858bda17a2f6fa03
Johan Dewe <johan.dewe@fritidsresor.se>
2014-06-10 22:13:24 +0200

    Render associations from api.

---
+++ js/perch.js
@@ -0,0 +1,12 @@
+﻿var perch = perch || {};
+
+perch.loadAssociations = function(element) {
+    console.log(element);
+
+    $.getJSON('api/', function (data) {
+
+        console.log(data);
+
+
+    });
+}
