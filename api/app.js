0fb3d42ede806e6f119d87c72ab44769d42a1f85
Johan Dewe <johan.dewe@fritidsresor.se>
2014-06-10 09:56:17 +0200

    Add backend call without authenticion.

--- api/app.js
+++ api/app.js
@@ -1,13 +1,21 @@
-var fs = require('fs'),
+var request = require('request'),
     express = require('express'),
     app = express();
 
+var config = {};
+config.associationsUrl = "https://ethersource.gavagai.se/ethersource/rest/findAssociations?apiKey=5TrKUetB36uA&customerObserverId=452&timestamp=2014-05-08%2000:00:00%20CEST&maxResults=30&windowSize=24&userId=gavagaiApplicant";
+
 app.get('/', function (req, res) {
 
-  fs.readFile(__dirname + '/test/trending_sample.json', 'utf8', function(err, text) {
-    var data = JSON.parse(text);
-    res.json(200, data);
-  });
+	request(config.associationsUrl, function (error, response, body) {
+	  	if (!error && response.statusCode == 200) {
+	  		var data = JSON.parse(body);
+  			res.json(data);
+	  	} else {
+	  		res.send({ "status": response.statusCode, "error": error })
+	  	}
+
+	});
 
 });
 
