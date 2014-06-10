db933ed5b47efe58841f96e91427c82eca3f537d
Johan Dewe <johan.dewe@fritidsresor.se>
2014-06-10 12:16:17 +0200

    Parallel async backend calls.

--- api/app.js
+++ api/app.js
@@ -1,32 +1,64 @@
 var config = require('./config.js')
+	async = require('async'),
 	request = require('request'),
     express = require('express'),
     app = express();
 
 var authRequest = request.defaults({
-		'auth': {
-			'user': config.userid,
-			'pass': config.passwd
+	'auth': {
+		'user': config.userid,
+		'pass': config.passwd
 		}
 	});
 
-app.get('/', function (req, res) {
+function mapAssociations(associationJson){
+	var associations = [];
+	var topics = associationJson.associationSearchResponse.associationsTopics;
+	for (var i = 0; i < topics.length; i++) {
+		topicAssociations = topics[i].associations;
+		associations.push.apply(associations, topicAssociations);
+	};
+	return associations;
+}
 
+function mapBackgroundAssociations(backgroundAssociationsJson){
+	return backgroundAssociationsJson.ethersourceBackgroundAssociationSearchResponse.backgroundAssociations;
+}
+
+function getAssociationsAsync(callback) {
 	authRequest(config.associationsUrl, function (error, response, body) {
 	  	if (!error && response.statusCode == 200) {
-	  		var data = JSON.parse(body);
-  			res.json(data);
+	  		var associations = mapAssociations(JSON.parse(body));
+	  		callback(null, associations);
 	  	} else {
-	  		res.json({ 
-	  			'message': 'backend request failed',
-	  			'statusCode': response.statusCode,
-	  			'error': error 
-	  		});
+	  		callback({ 'msg': 'backend request failed', 'statusCode': response.statusCode, 'error': error });
 	  	}
 	});
+};
+
+function getBackgroundAssociationsAsync(callback) {
+	authRequest(config.backgroundAssociationsUrl, function (error, response, body) {
+	  	if (!error && response.statusCode == 200) {
+	  		var associations = mapBackgroundAssociations(JSON.parse(body));
+	  		callback(null, associations);
+	  	} else {
+	  		callback({ 'msg': 'backend request failed', 'statusCode': response.statusCode, 'error': error });
+	  	}
+	});
+};
+
+app.get('/', function (req, res) {
+
+	async.parallel({
+			'trending': getAssociationsAsync,
+			'stable': getBackgroundAssociationsAsync
+		}, function(err, result) {
+			res.json(result);
+		});
+
 });
 
 // start server
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 app.listen(port);
 console.log('Listening on port ' + port);
